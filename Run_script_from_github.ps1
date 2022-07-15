162d331578af3e572b8519f404135a305e3f1b40
AnshulAwasthi21 <43340091+AnshulAwasthi21@users.noreply.github.com>
2022-07-16 04:05:41 +0530

    + Raw Url builder to execute script from Github
    The script has a new feature to create a raw URL from the normal Github URL

--- Run_script_from_github.ps1
+++ Run_script_from_github.ps1
@@ -4,11 +4,16 @@ Example url - https://github.com/AnshulAwasthi21/pers_ref/blob/main/sample_scrip
 url to use with the Invoke method cmdlet - https://raw.githubusercontent.com/AnshulAwasthi21/pers_ref/main/sample_script.ps1
 #>
 
-$githubscript = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/AnshulAwasthi21/pers_ref/main/sample_script.ps1'
+# Provide the git hub url to grab a script from a public repository 
+$url = 'https://github.com/AnshulAwasthi21/pers_ref/blob/main/sample_script.ps1' 
+
+# Raw url builder
+$rawurl = $url.Replace('github.com','raw.githubusercontent.com').Replace('blob/','')
+
+$rawscripturl = Invoke-WebRequest -Uri "$rawurl"
 
 # The variable $ps1content will contain the script content
-$ps1content = ($githubscript).Content
+$ps1content = ($rawscripturl).Content
 
-#In order to execute the script directly from the github repo run the below cmdlet 
-
+#In order to execute the script directly from the github repo 
 Invoke-Expression $ps1content
